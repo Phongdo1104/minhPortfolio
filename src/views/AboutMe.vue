8d53c00659dc635cffd0fe0ac2d658de38856b3c
Phong Kim Do <kimphongdo1104@gmail.com>
2024-10-08 20:59:14 +0700

    Contact-me-page - Update contact page, footer and header block upgrade

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -3,88 +3,98 @@ import '../assets/about-me.css'
 </script>
 
 <template>
-  <div>
-    <div class="desk-angle-1">
-      <div class="container pb-5" id="about-me">
-        <div class="row">
-          <div class="col-md mb-5">
-            <div class="position-relative d-flex justify-content-center">
-              <img
-                class="avatar img-fluid"
-                src="/images/minh2-edited.png"
-                alt="MinhDarkBG"
-              />
-              <h1
-                class="position-absolute text-white fw-bold display-1 text-overlay text-center text-uppercase"
-              >
-                About me
-              </h1>
+    <div>
+        <div class="desk-angle-1">
+            <div class="container pb-5" id="about-me">
+                <div class="row">
+                    <div class="col-md mb-5">
+                        <div class="position-relative d-flex justify-content-center">
+                            <img class="avatar img-fluid" src="/images/minh2-edited.png" alt="MinhDarkBG" />
+                            <h1
+                                class="position-absolute text-white fw-bold display-1 text-overlay text-center text-uppercase">
+                                About me
+                            </h1>
+                        </div>
+                    </div>
+                    <div class="col-md me-5">
+                        <div class="text-white">
+                            <div class="text-end" id="nameHeader">
+                                <span class="fw-bold" id="fullName">Minh Nguyen</span>
+                            </div>
+                            <div class="nav-bar mt-4 mb-5"></div>
+                            <div class="mb-5">
+                                <p class="h5 fw-bold">
+                                    Driven by a passion for digital branding, I am a 24-year-old self-taught social
+                                    media video editor.
+                                </p>
+                            </div>
+                            <div class="mb-5">
+                                <p class="h5">
+                                    My focus is on enhancing digital brands with creative professionalism rooted in
+                                    clients' original stories.
+                                </p>
+                            </div>
+                            <div class="mb-2">
+                                <p class="h5">I am proficient in the following tools:</p>
+                            </div>
+                            <div class="row col-auto d-flex justify-content-start mb-5" id="tools">
+                                <div class="col-auto">
+                                    <img class="logo-tools img-fluid" src="/images/PRIcons.png" alt="PremierIcon" />
+                                </div>
+                                <div class="col-auto ms-3">
+                                    <img class="logo-tools img-fluid" src="/images/AFIcons.png" alt="PremierIcon" />
+                                </div>
+                                <div class="col-auto ms-3">
+                                    <img class="logo-tools img-fluid" src="/images/CapCut_logoIcons.png"
+                                        alt="PremierIcon" />
+                                </div>
+                            </div>
+                            <div class="mb-5 h5" id="experience">
+                                <p>Until 2024, I have gained 4 years of experience in the following areas:</p>
+                                <ul class="fw-bold">
+                                    <li class="mb-2">Video production & editing</li>
+                                    <li class="mb-2">Social media marketing</li>
+                                    <li class="mb-2">Filmmaking & backstage work</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="achievements" class="row">
+                    <div class="col-sm text-center text-white video-editing-block">
+                        <div class="text-uppercase title-achievements h2">
+                            Video Editing
+                        </div>
+                        <div class="achievements-count">4</div>
+                        <div class="achievements-description h5">
+                            Since 2020, I've self-taught and edited a range of videos, including tutorials,
+                            documentaries, K.O.P., and TVCs.</div>
+                    </div>
+                    <div class="col-sm text-white d-flex align-items-center">
+                        <a href="#">
+                            <img class="show-reel img-fluid" src="/svg/Showreel.svg" alt="showReelButton">
+                        </a>
+                    </div>
+                    <div class="col-sm text-center text-white marketing-block">
+                        <div class="text-uppercase title-achievements h2">
+                            Social Media Marketing
+                        </div>
+                        <div class="achievements-count">2</div>
+                        <div class="achievements-description h5">
+                            In 2022, I started as a SM marketer and centered my creativity based on understanding SNS
+                            platforms
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="col-md me-5">
-            <div class="text-white">
-              <div class="text-end" id="nameHeader">
-                <span class="fw-bold display-1" id="fullName">Minh Nguyen</span>
-              </div>
-              <div class="nav-bar mt-4 mb-5"></div>
-              <div class="mb-5">
-                <p class="h5 fw-bold">
-                  Driven by a passion for digital branding, I am a 24-year-old self-taught social
-                  media video editor.
-                </p>
-              </div>
-              <div class="mb-5">
-                <p class="h5">
-                  My focus is on enhancing digital brands with creative professionalism rooted in
-                  clients' original stories.
-                </p>
-              </div>
-              <div class="mb-2">
-                <p class="h5">I am proficient in the following tools:</p>
-              </div>
-              <div class="row col-auto d-flex justify-content-start mb-5" id="tools">
-                <div class="col-auto">
-                  <img
-                    class="logo-tools img-fluid"
-                    src="/images/PRIcons.png"
-                    alt="PremierIcon"
-                  />
+            <div id="scroll-text">
+                <div class="marquee branding-text text-center text-uppercase">
+                    <p class="h1 me-3">Content Creation Visual Storytelling Flimmaking Video Editing Digital Branding
+                        Backstage Professionalism Premiere Pro After Effects Cap Cut</p>
+                    <p class="h1">Content Creation Visual Storytelling Flimmaking Video Editing Digital Branding
+                        Backstage Professionalism Premiere Pro After Effects Cap Cut</p>
                 </div>
-                <div class="col-auto ms-3">
-                  <img
-                    class="logo-tools img-fluid"
-                    src="/images/AFIcons.png"
-                    alt="PremierIcon"
-                  />
-                </div>
-                <div class="col-auto ms-3">
-                  <img
-                    class="logo-tools img-fluid"
-                    src="/images/CapCut_logoIcons.png"
-                    alt="PremierIcon"
-                  />
-                </div>
-              </div>
-              <div class="mb-5 h5" id="experience">
-                <p>Until 2024, I have gained 4 years of experience in the following areas:</p>
-                <ul class="fw-bold">
-                  <li class="mb-2">Video production & editing</li>
-                  <li class="mb-2">Social media marketing</li>
-                  <li class="mb-2">Filmmaking & backstage work</li>
-                </ul>
-              </div>
-              <div class="border-top border-bottom border-start p-5">
-                <img
-                  class="certificate img-fluid"
-                  src="/images/cert.png"
-                  alt="certificate"
-                  id="certificate"
-                />
-              </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
--- src/views/HeaderPage.vue
+++ src/views/HeaderPage.vue
@@ -1,32 +1,122 @@
 <script lang="ts" setup>
 import '../assets/header-page.css'
-</script>
+import { ref } from 'vue'
 
+var show = ref(false);
+
+function closePopup() {
+    this.show = false;
+}
+
+</script>
 <template>
-  <div class="row">
-    <div class="col-md">
-      <RouterLink to="/">
-        <img id="logo-home-page" src="/images/Logo_LightIcon.png" alt="" />
-      </RouterLink>
+    <div class="d-block d-md-none header-sp fixed-header" id="fixedHeader">
+        <div class="position-absolute top-0 start-0 ms-3">
+            <RouterLink to="/">
+                <img id="logo-home-page" src="/images/Logo_LightIcon.png" alt="" />
+            </RouterLink>
+        </div>
+        <div class="position-absolute top-0 end-0 mt-3 me-3">
+            <a href="#" @click="show = !show">
+                <img id="menu-icon" src="/svg/menu.svg" alt="" />
+            </a>
+        </div>
     </div>
-    <div class="col-auto row align-self-center">
-      <div class="d-none d-md-block" id="header-links">
-        <RouterLink to="/" class="text-decoration-none fw-bold text-center" id="home-page">
-          Home
-        </RouterLink>
-        <RouterLink to="/about-me" class="text-decoration-none ms-5 fw-bold" id="about-me">
-          About Me
-        </RouterLink>
-        <RouterLink to="/about-me" class="text-decoration-none ms-5 fw-bold" id="my-projects">
-          My Projects
-        </RouterLink>
-        <RouterLink to="/about-me" class="text-decoration-none ms-5">
-          <button id="get-in-touch" class="ps-5 pt-2 pb-2 pe-5 fw-bold">Get In Touch</button>
-        </RouterLink>
-      </div>
-      <a class="col-auto d-block d-md-none position-absolute top-0 end-0 mt-3">
-        <img id="menu-icon" src="/svg/menu.svg" alt="" />
-      </a>
+    <div class="container-sm d-none d-md-block">
+        <div class="row">
+            <div class="col-md">
+                <RouterLink to="/">
+                    <img id="logo-home-page" src="/images/Logo_LightIcon.png" alt="" />
+                </RouterLink>
+            </div>
+            <div class="col-auto row align-self-center">
+                <div class="d-none d-md-block" id="header-links">
+                    <RouterLink to="/" class="header-btn text-decoration-none fw-bold text-center" id="home-page">
+                        Home
+                    </RouterLink>
+                    <RouterLink to="/about-me" class="header-btn text-decoration-none ms-5 fw-bold" id="about-me">
+                        About Me
+                    </RouterLink>
+                    <RouterLink to="/about-me" class="header-btn text-decoration-none ms-5 fw-bold" id="my-projects">
+                        My Projects
+                    </RouterLink>
+                    <RouterLink to="/contact-me" class="text-decoration-none ms-5">
+                        <button id="get-in-touch" class="ps-5 pt-2 pb-2 pe-5 fw-bold">Get In Touch</button>
+                    </RouterLink>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+    <transition :duration="450" name="nested">
+        <div v-if="show" class="d-block d-md-none h-100 position-fixed align-items-center h1 sidebar-navigation sidebar-block pt-5 h-100 d-flex justify-content-center align-items-center fixed-header-text"
+            id="fixedSidebar" data-toggle="animation" data-animation-reset="true" data-animation="slide-right">
+            <div class="pt-5 h-100 d-flex justify-content-center align-items-center">
+                <div class="fixed-header-text">
+                    <RouterLink to="/" @click="closePopup()" class="mb-3 text-white text-decoration-none text-center sidebar-item w-100">
+                        Home
+                    </RouterLink>
+                    <RouterLink to="/about-me" @click="closePopup()" class="mb-3 text-white text-decoration-none text-center sidebar-item w-100">
+                        About me
+                    </RouterLink>
+                    <RouterLink to="/about-me" @click="closePopup()" class="mb-3 text-white text-decoration-none text-center sidebar-item w-100">
+                        My Projects
+                    </RouterLink>
+                    <RouterLink to="/contact-me" @click="closePopup()"
+                        class="mb-3 text-white text-decoration-none text-center sidebar-item w-100">
+                        Get in touch
+                    </RouterLink>
+                </div>
+            </div>
+        </div>
+    </transition>
 </template>
+<style>
+.outer,
+.inner {
+    background: #eee;
+    padding: 30px;
+    min-height: 100px;
+}
+
+.inner {
+    background: #ccc;
+}
+
+.nested-enter-active,
+.nested-leave-active {
+    transition: all 0.15s ease-in-out;
+}
+
+/* delay leave of parent element */
+.nested-leave-active {
+    transition-delay: 0.05s;
+}
+
+.nested-enter-from,
+.nested-leave-to {
+    transform: translateY(30px);
+    opacity: 0;
+}
+
+/* we can also transition nested elements using nested selectors */
+.nested-enter-active .inner,
+.nested-leave-active .inner {
+    transition: all 0.15s ease-in-out;
+}
+
+/* delay enter of nested element */
+.nested-enter-active .inner {
+    transition-delay: 0.1s;
+}
+
+.nested-enter-from .inner,
+.nested-leave-to .inner {
+    transform: translateX(30px);
+    /*
+  	Hack around a Chrome 96 bug in handling nested opacity transitions.
+    This is not needed in other browsers or Chrome 99+ where the bug
+    has been fixed.
+  */
+    opacity: 0.001;
+}
+</style>
