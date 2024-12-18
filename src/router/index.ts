633c4b9402ff3841a4fa4cd90d526efc65cdf1df
Phong Kim Do <kimphongdo1104@gmail.com>
2024-12-18 22:22:59 +0700

    Home-page - Update page title for each page and format code

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,33 +5,44 @@ import ContactMe from '../views/ContactMe.vue'
 import MyWorks from '../views/MyWorksPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomePage,
-      meta: { transition: '' } // 'fade'
-    },
-    {
-      path: '/about-me',
-      name: 'about-me',
-      component: AboutMe,
-      meta: { transition: '' } // slide
-    },
-    {
-      path: '/my-works',
-      name: 'my-works',
-      component: MyWorks,
-      meta: { transition: '' } // slide
-    },
-    {
-      path: '/contact-me',
-      name: 'contact-me',
-      component: ContactMe,
-      meta: { transition: '' } // slide
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomePage,
+            meta: { transition: '' } // 'fade'
+        },
+        {
+            path: '/about-me',
+            name: 'about-me',
+            component: AboutMe,
+            meta: { transition: '' } // slide
+        },
+        {
+            path: '/my-works',
+            name: 'my-works',
+            component: MyWorks,
+            meta: { transition: '' } // slide
+        },
+        {
+            path: '/contact-me',
+            name: 'contact-me',
+            component: ContactMe,
+            meta: { transition: '' } // slide
+        }
+    ]
 })
+
+router.beforeEach((to: any, from, next) => {
+    let title = to.name;
+    title = title.replace("-", " ");
+    title = title.toLowerCase()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+    document.title = title;
+    next();
+});
 
 export default router
